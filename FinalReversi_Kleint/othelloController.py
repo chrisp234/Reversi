def43fc345fd3a0c05fb7cedbba261676b50ef55
chrisp234 <123581648+chrisp234@users.noreply.github.com>
2023-03-04 15:51:33 -0500

    Update othelloController.py

--- FinalReversi_Kleint/othelloController.py
+++ FinalReversi_Kleint/othelloController.py
@@ -15,30 +15,37 @@ class othelloController:
         #loops until no valid moves (or board fills)
         while True:
             self.view.display_board(self.model.board)
-            valid_moves = self.model.get_valid_moves()
-            print("Valid Moves: " + str(valid_moves))
-            #if no valid moves for player1-> check if player2 has moves and continue loop
-            if len(valid_moves) == 0:
-                if player_noMoves:
-                    break
+            gameType = input("Type 'local' for local game or 'cpu' for AI controlled opponent")
+            if gameType == "local":
+                valid_moves = self.model.get_valid_moves()
+                print("Valid Moves: " + str(valid_moves))
+                #if no valid moves for player1-> check if player2 has moves and continue loop
+                if len(valid_moves) == 0:
+                    if player_noMoves:
+                        break
+                    self.model.change_player()
+                    player_noMoves = True
+                    continue
+                print(f"Player {self.model.get_curr_player()}'s turn")
+                row, col = self.view.get_move()
+                if (row, col) not in valid_moves:
+                    print("Invalid move")
+                    continue
+                self.model.make_move(row, col)
                 self.model.change_player()
-                player_noMoves = True
-                continue
-            print(f"Player {self.model.get_curr_player()}'s turn")
-            row, col = self.view.get_move()
-            if (row, col) not in valid_moves:
-                print("Invalid move")
-                continue
-            self.model.make_move(row, col)
-            self.model.change_player()
+            
+                #find/displays winner
+                winner = self.model.get_winner()
+                self.view.display_board(self.model.board)
+                self.view.display_winner(winner)
+            elif gameType == "cpu":
+                break;
+            else:
+                print("Not a valid option")
 
-        #find/displays winner
-        winner = self.model.get_winner()
-        self.view.display_board(self.model.board)
-        self.view.display_winner(winner)
 
 
 if __name__ == "__main__":
-    size = 4
+    size = 8
     controller = othelloController(size)
     controller.play_game()
