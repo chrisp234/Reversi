40f97b231f5a5acc6d58611828df6d607844b31f
ckleint <111015683+ckleint@users.noreply.github.com>
2023-03-21 16:25:20 -0400

    Update database.py

--- FinalReversi_Kleint/database.py
+++ FinalReversi_Kleint/database.py
@@ -14,120 +14,91 @@ class Database():
                 port = 5432
             )
 
-            cursor = self.conn.cursor()
+            self.cursor = self.conn.cursor()
         except:
             pass
-            
+
     def setup(self):
         setup_text = '''CREATE TABLE IF NOT EXISTS player (
-                        id int PRIMARY KEY, 
-                        username varchar(100) default NULL, 
-                        password varchar(100) default NULL, 
-                        ELO int 
+                        id SERIAL PRIMARY KEY, 
+                        username VARCHAR(100) NOT NULL UNIQUE, 
+                        password VARCHAR(100) NOT NULL, 
+                        ELO INT 
                         ); 
 
                         CREATE TABLE IF NOT EXISTS game (
-                        gid int PRIMARY KEY,
-                        timestamp_start varchar(100),
-                        timestamp_end varchar(100),
-                        final_score int default NULL,
-                        winner_ID int,
-                        size_val int
+                        gid SERIAL PRIMARY KEY,
+                        timestamp_start TIMESTAMP,
+                        timestamp_end TIMESTAMP,
+                        final_score INT DEFAULT NULL,
+                        winner_ID INT REFERENCES player(id),
+                        size_val INT
                         );
 
                         CREATE TABLE IF NOT EXISTS player_game (
-                        playerID int REFERENCES player(id),
-                        gameID int REFERENCES game(gid),
-                        player_color varchar(100),
+                        playerID INT REFERENCES player(id),
+                        gameID INT REFERENCES game(gid),
+                        player_color VARCHAR(100),
                         PRIMARY KEY(playerID, gameID)
                         );
 
-
                         CREATE TABLE IF NOT EXISTS board_states (
-                        sid int PRIMARY KEY,
-                        gid int REFERENCES game(gid),
-                        move_num varchar(100),
-                        board_state varchar(1000),
-                        player_color_turn varchar(100)
+                        sid SERIAL PRIMARY KEY,
+                        gid INT REFERENCES game(gid),
+                        move_num INT,
+                        board_state VARCHAR(1000),
+                        player_color_turn VARCHAR(100)
                         );'''
-        cursor = self.conn.cursor()
-        cursor.execute(setup_text)
-        pass
+        self.cursor.execute(setup_text)
+        self.conn.commit()
 
 
     def verify_username_password(self, username, password):
-        print(username)
-        print(password)
-        verify_text="select player.username, player.password from player where player.username=%s and player.password=%s"
-        cursor = self.conn.cursor()
-        cursor.execute(verify_text, (username, password))
-        print(cursor.fetchall())
-        if cursor.rowcount != 0:
+        verify_text = "SELECT player.username, player.password FROM player WHERE player.username=%s AND player.password=%s"
+        self.cursor.execute(verify_text, (username, password))
+        result = self.cursor.fetchall()
+
+        if self.cursor.rowcount != 0:
+            return True
+        else:
+            return False
+        
+    def verify_username(self, username):
+        verify_text = "SELECT * FROM player WHERE player.username=%s"
+        self.cursor.execute(verify_text, username)
+        result = self.cursor.fetchall()
+
+        if self.cursor.rowcount == 0:
             return True
         else:
             return False
 
-    def verify_password(self):
-        pass
-
-    def insert_newPlayer(self):
-        cursor = self.conn.cursor()
-        cursor.execute("insert into player values(00003, 'bob, 'password123', 1500);")
+    def insert_new_player(self, username, password):
+        insert_text = "INSERT INTO player (username, password, elo) VALUES (%s, %s, %s);"
+        self.cursor.execute(insert_text, (username, password, 1500))
         self.conn.commit()
 
-    def execute(self):
-        cursor = self.conn.cursor()
-        cursor.execute("insert into player values(00002, 'joe', 'password123', 504);")
+    def update_elo(self, player_id, new_elo):
+        update_text = "UPDATE player SET elo = %s WHERE id = %s;"
+        self.cursor.execute(update_text, (new_elo, player_id))
         self.conn.commit()
-        print(cursor.execute("Select * From Player" ))
+
+    def get_all_players(self):
+        select_text = "SELECT * FROM player;"
+        self.cursor.execute(select_text)
+        return self.cursor.fetchall()
+
+    def get_player_by_id(self, player_id):
+        select_text = "SELECT * FROM player WHERE id = %s;"
+        self.cursor.execute(select_text, (player_id,))
+        return self.cursor.fetchone()
 
     def close(self):
+        self.cursor.close()
         self.conn.close()
 
-    #expect score of A in a match against B
     def expected(self, A, B):
         return 1 / (1 + 10 ** ((B - A) / 400))
 
-    #calculate new ELO rating for player A
     def elo(self, old_score, expected, actual_score, k_factor):
         return old_score + k_factor * (actual_score - expected)
-
-
-'''
-
----------------------------
--- Create the tables
----------------------------
-
-CREATE TABLE IF NOT EXISTS player (
-id int PRIMARY KEY, 
-username varchar(100) default NULL, 
-password varchar(100) default NULL, 
-ELO int 
-); 
-
-CREATE TABLE IF NOT EXISTS game (
-  gid int PRIMARY KEY,
-  timestamp_start varchar(100),
-  timestamp_end varchar(100),
-  final_score int default NULL,
-  winner_ID int,
-  size_val int
-);
-
-CREATE TABLE IF NOT EXISTS player_game (
-  playerID int REFERENCES player(id),
-  gameID int REFERENCES game(gid),
-  player_color varchar(100),
-  PRIMARY KEY(playerID, gameID)
-);
-
-
-CREATE TABLE IF NOT EXISTS board_states (
-  sid int PRIMARY KEY,
-  gid int REFERENCES game(gid),
-  move_num varchar(100),
-  board_state varchar(1000),
-  player_color_turn varchar(100)
-);
-'''
