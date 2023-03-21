dee331782b1577b0a09d9fe86bf2feaaba5f7f9a
chrisp234 <123581648+chrisp234@users.noreply.github.com>
2023-03-21 14:13:38 -0400

    Added AI Functionality

--- FinalReversi_Kleint/othelloController.py
+++ FinalReversi_Kleint/othelloController.py
@@ -1,5 +1,6 @@
 import othelloGame
 import othelloView
+import copy
 import numpy as np
 
 class othelloController:
@@ -8,10 +9,44 @@ class othelloController:
         self.model =  othelloGame.othelloGame(size)
         self.view = othelloView.othelloView()
 
+    def minmax(self, model, depth, alpha=-np.inf, beta=np.inf):
+        if depth == 0 or self.model.get_valid_moves() == 0:
+            return None, 0
+
+        valid_moves = self.model.get_valid_moves()
+
+        if self.model.get_curr_player() == 2:
+            best_score = np.inf
+            best_move = None
+            for move in valid_moves:
+                model_copy = copy.deepcopy(self.model)
+                model_copy.make_move(move[0], move[1])
+                _, score = self.minmax(model_copy, depth-1, alpha, beta)
+                if score < best_score:
+                    best_score = score
+                    best_move = move
+                beta = min(beta, score)
+                if alpha >= beta:
+                    break
+        else:
+            best_score = -np.inf
+            best_move = None
+            for move in valid_moves:
+                model_copy = copy.deepcopy(self.model)
+                model_copy.make_move(move[0], move[1])
+                _, score = self.minmax(model_copy, depth-1, alpha, beta)
+                if score > best_score:
+                    best_score = score
+                    best_move = move
+                alpha = max(alpha, score)
+                if alpha >= beta:
+                    break
+        return best_move
+
     #plays the game. No actual logic here
     def play_game(self):
         player_noMoves = False
-        gameType = input("Type 'local' for local game or 'cpu' for AI controlled opponent")
+        gameType = input("Type 'local' for local game or 'cpu' for AI controlled opponent : \n")
         if gameType == "local":
             while True:
                 self.view.display_board(self.model.board)
@@ -36,13 +71,34 @@ class othelloController:
             self.view.display_board(self.model.board)
             self.view.display_winner(winner)
         elif gameType == "cpu":
-            exit
+            while True:
+                self.view.display_board(self.model.board)
+                valid_moves = self.model.get_valid_moves()
+                print("Valid Moves: " + str(valid_moves))
+                if len(valid_moves) == 0:
+                    self.model.change_player()
+                    valid_moves = self.model.get_valid_moves()
+                if len(valid_moves) == 0:
+                    break
+                if self.model.get_curr_player() == 1:
+                    print(f"Player {self.model.get_curr_player()}'s turn")
+                    row, col = self.view.get_move()
+                    if (row, col) not in valid_moves:
+                        print("Invalid move")
+                        continue
+                    self.model.make_move(row, col)
+                else:
+                    print(f"CPU's turn")
+                    row, col = self.minmax(self.model, 3)
+                    self.model.make_move(row, col)
+                self.model.change_player()
+            winner = self.model.get_winner()
+            self.view.display_board(self.model.board)
+            self.view.display_winner(winner)
         else:
            print("Not a valid option")
 
-
-
 if __name__ == "__main__":
-    size = 8
-    controller = othelloController(size)
+    size = input("Please enter board size : ")
+    controller = othelloController(int(size))
     controller.play_game()
