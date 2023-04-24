3303a8eaf0f33b4644990f22fb335a1bfa60db2a
Cristine Kleint <cristinekleint@Cristines-Air.hsd1.ma.comcast.net>
2023-04-24 13:59:44 -0400

    Changes

--- reversi-backend/src/server.ts
+++ reversi-backend/src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 const cookieParser = require("cookie-parser");
-
+const path = require('path');
 import { makeGameController } from './controllers/GameController';
 import { makeSessionsController } from './controllers/SessionsController';
 import { makeInvitationsController } from './controllers/InvitationsController';
@@ -10,6 +10,7 @@ const app = express()
 const PORT = process.env['PORT'] ?? 3333
 app.use(express.json())
 app.use(cookieParser())
+app.use(express.static('dist', {index: 'index.html'}))
 
 makeGameController(app);
 makeSessionsController(app);
@@ -17,4 +18,8 @@ makeInvitationsController(app);
 makeLeaderboardController(app);
 
 console.log(`Starting on port ${PORT}`);
+app.get("*", (req, res, next) => {
+    console.log("in catch all")
+    res.sendFile(path.join(__dirname, "../dist/index.html"))
+})
 app.listen(PORT)
